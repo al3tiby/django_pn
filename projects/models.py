001b4a8c01c13c001e2cbbfadb1fdfb1e3879986
al3tiby <al3tiby2003@gmail.com>
2022-09-15 23:16:00 +0300

    Fixing the category __str__ returning problem

--- projects/models.py
+++ projects/models.py
@@ -6,19 +6,19 @@ from django.conf.global_settings import AUTH_USER_MODEL
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    
-    def __str(self):
+
+    def __str__(self):
         return self.name
-    
-    
+
+
 class ProjectStatus(models.IntegerChoices):
     PENDING = 1, 'Pending'
     COMPLETED = 2, 'Completed'
     POSTPONED = 3, 'Postponed'
     CANCELED = 4, 'Canceled'
-    
 
-    
+
+
 class Project(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -33,18 +33,19 @@ class Project(models.Model):
         AUTH_USER_MODEL,
         on_delete=models.CASCADE
         )
-    
+
     def __str__(self):
         return self.title
 
-    
-    
-    
+
+
+
+
 class Task(models.Model):
     description = models.TextField()
     is_completed = models.BooleanField(default=False)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return self.description
-    
+
