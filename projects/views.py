8bebf0c5d0dc9d532286131505199ee873a88ae2
al3tiby <al3tiby2003@gmail.com>
2022-09-17 20:35:47 +0300

    .

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import ListView, CreateView, UpdateView
-from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
 from . import models, forms
 from django.shortcuts import render
 
@@ -22,4 +22,42 @@ class ProjectUpdateView(UpdateView):
     model = models.Project
     form_class = forms.ProjectUpdateForm
     template_name = 'project/update.html'
+
+    def get_success_url(self):
+        return reverse('Project_update', args=[self.object.id])
+
+
+
+class ProjectDeleteView(DeleteView):
+    model = models.Project
+    template_name = 'project/project_confirm_delete.html'
     success_url = reverse_lazy('Project_list')
+
+
+
+class TaskCreateView(CreateView):
+    model = models.Task
+    fields = ['project', 'description']
+    http_method_names = ['post']
+
+    def get_success_url(self):
+        return reverse('Project_update', args=[self.object.project.id])
+
+
+
+class TaskUpdateView(CreateView):
+    model = models.Task
+    fields = ['is_completed']
+    http_method_names = ['post']
+
+    def get_success_url(self):
+        return reverse('Project_update', args=[self.object.project.id])
+
+
+
+class TaskDeleteView(DeleteView):
+    model = models.Task
+
+    def get_success_url(self):
+        return reverse('Project_update', args=[self.object.project.id])
+
