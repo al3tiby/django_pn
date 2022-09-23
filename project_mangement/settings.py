525891f96db9956b064637fdf259be03dfc86420
al3tiby <al3tiby2003@gmail.com>
2022-09-23 11:09:24 +0300

    setting superuser account

--- project_mangement/settings.py
+++ project_mangement/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiiles'),)
 
 
 # Default primary key field type
