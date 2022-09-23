0b8b20a0414aaf4edb0e7493111cd6021148c5de
al3tiby <al3tiby2003@gmail.com>
2022-09-23 14:15:29 +0300

    installing packages

--- project_mangement/settings.py
+++ project_mangement/settings.py
@@ -131,6 +131,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiiles'),)
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
